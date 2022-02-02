44d979f052ef3cad9b7f4a7f1d0df8b47a61cd5a
raheemadamboev <raheemideadev@gmail.com>
2022-02-02 18:29:00 +0500

    dependencies setup

--- app/src/main/java/xyz/teamgravity/kotlinflows/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/kotlinflows/MainActivity.kt
@@ -3,6 +3,7 @@ package xyz.teamgravity.kotlinflows
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
