fa677a9e19090bda9c6b72d0bd0afa7992cff7bb
raheemadamboev <raheemideadev@gmail.com>
2022-02-04 14:18:08 +0500

    simple flow operators -> filter, map, onEach, launchIn

--- app/src/main/java/xyz/teamgravity/kotlinflows/MainViewModel.kt
+++ app/src/main/java/xyz/teamgravity/kotlinflows/MainViewModel.kt
@@ -3,8 +3,7 @@ package xyz.teamgravity.kotlinflows
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
@@ -21,16 +20,31 @@ class MainViewModel : ViewModel() {
     }
 
     init {
-        collectCountdown()
+        //collectCountdown()
+        simpleFlowOperators()
     }
 
     private fun collectCountdown() {
         viewModelScope.launch {
             // damn they don't collect same flow, two different flow count down started as i delayed collecting this one
             // one for ui collecting as state, one for viewmodel itself, two different count down?
-            delay(1_000)
+            delay(2_000)
             countDownFlow.collect { count ->
                 println("raheem: $count")
+            }
+        }
+    }
+
+    private fun simpleFlowOperators() {
+        viewModelScope.launch {
+            countDownFlow.filter { time ->
+                time % 2 == 0
+            }.map { time ->
+                time * time
+            }.onEach { time ->
+                println("raheem: $time onEach")
+            }.collect { time ->
+                println("raheem: $time collect")
             }
         }
     }
